d41ff639de3b305e6f9c7cdbb460e90534b29201
Vishes77 <42653296+Vishes77@users.noreply.github.com>
2020-03-26 18:28:25 +0530

    int main() part
    Did The main() Part

--- question8.c
+++ question8.c
@@ -1,7 +1,35 @@
 #include<stdio.h>
 #include<string.h>
 
+
 int main()
 {
-
+	int type;
+	int available_process;
+	printf("     ENTER 1 OR 2      ");
+	printf("For STUDENT -> Enter 1");
+	printf("For FACULTY -> Enter 2");
+	scanf("%d",&type);
+	
+	switch(type)
+	{
+		case 1:
+			printf("   STUDENT TYPE IS SELECTED   ");
+			printf("\n\n");
+			printf("Enter No of Process for STUDENT : ");
+			scanf("%d",&available_process);
+			forfaculty(available_process);
+			break;
+		case 2:
+			printf("   FACULTY TYPE IS SELECTED   ");
+			printf("\n\n");
+			printf("Enter No of Process for FACULTY : ");
+			scanf("%d",&available_process);
+			forfaculty(available_process);	
+			break;
+		default:
+			printf("Wrong option Selected Sorry");
+			printf("Re-Run AGAIN");			
+	}
+	return 0;	
 }
