63045667a118f9710c08e98dbf780da5380d16e3
Vishes77 <42653296+Vishes77@users.noreply.github.com>
2020-03-29 21:29:53 +0530

    Rewitten main and input part

--- question8.c
+++ question8.c
@@ -1,264 +1,78 @@
 #include<stdio.h>
-#include<string.h>
 
-struct avial_process
-{
-	char pname[30];
-	int av_t,br_t,com_y,rem;
-}obj;
-
-void forfaculty(int av_process)
-{
-	int total,av_t,br_t,qun;
-	struct avial_process fac[av_process];
-	
-	for(total = 0;total <av_process;total++){
-		printf("Process Detail for Process[%d]",total+1);
-		printf("\n");
-		printf("Enter name of Process : ");
-		scanf("%s",fac[total].pname);
-		printf("Enter ARRIVAL TIME : ");
-		scanf("%s",fac[total].av_t);
-		printf("Enter BRUST TIME : ");
-		scanf("%s",fac[total].br_t);	
-	}
-	printf("Enter QUANTUM TIME : ");
-	scanf("%d",&qun);// part b
-	for(total = 0;total <av_process;total++)
-	{
-		for(int j= total +1 ; j<total;total++)
-		{
-			if(fac[total].av_t > fac[j].av_t)
-			{
-				obj = fac[count];
-				fac[count] = fac[j];
-				fac[j] = obj;
-			}
-		}
-	}
-	//part b
-	for(total = 0;total <av_process ; total++){
-		fac[total].rem = fac[total].br_t;
-		fac{total}.com_y = 0;
-	}
-	//part c
-	int TT,Q,RR[20];
-	TT = 0;
-	Q = 0;
-	RR[Q] = 0;
-	
-	int FG,X,N,Z,WT=0;
-	do
-	{
-		for(total = 0;total<av_process;total++)
-		{
-			if(TT >= fac[av_pocess].av_t)
-			{
-				Z=0;
-				for(X=0;X<=Q;X++)
-				{
-					if(RR[X] == av_process)
-					{
-						Z++;
-					}
-				}
-				if(Z == 0)
-				{
-					Q++;
-					RR[Q] == av_process;
-				}	
-			}
-		}
-		if(Q == 0)
-		{
-			N = 0;
-		}
-		if(fac[av_process].rem == 0)
-		{
-			N++;
-		}
-		if(N>Q)
-		{
-			N = (N-1)%Q;
-		}
-		if(N<=Q)
-		{	if(fac[N].rem > 0)
-			{
-				if(fac[N].rem <qun)
-				{
-					TT += fac[N].rem;
-					fac[N].rem=0;
-				}
-				else
-				{
-					TT += qun;
-					fac[N].rem -= qun;
-				}
-				fac[N].com_y = TT;
-			}
-			N++;		
-		}
-		FG=0;
-		
-		for(count = 0; count < av_process; count++) 
-		{
-            if(fac[av_process].rem > 0) 
-			{
-                FG++;
+void takedata(){
+    int val,  i, avg;
+    printf("Enter total Process (IN NUMBERS): "); 
+    scanf("%d", &n);
+    if(n==0) 
+    { 
+        printf("\n You have Enterd 0 \n"); 
+    }
+    else
+    {
+        printf("\n Now Enter The QUANTUM TIME for each Process : "); 
+        scanf("%d", &quantamtime);
+        printf("\nFor FACULTY( PRESS 1 ) \n");
+        printf("\nFor STUDENT( PRESS 2 ) \n");
+        for(i=0; i<n; i++)
+        {
+            printf("\n YOUR CHOICE ( 1 or 2)  : "); 
+            scanf("%d", &val);
+            if(val==1)
+            {
+                printf("PLEASE ENTER VALUE IN NUMBERS(e.g 1 to 9)");
+                printf("ENTER Query Id : "); 
+                scanf("%d", &e[fc].processid);
+                printf("ENTER Arrival Time: "); 
+                scanf("%d", &avg);
+                if(avg<1000 || avg>1200)
+                {
+                    printf("WRITE IN FORMAT LIKE e.g 10:30  -> 1030 \n");
+                    printf("\n!!! WRONG !!! Enter Correct TIME");
+                    takedata();
+                }
+                else
+                {
+                    e[fc].timea= avg-1000;
+                }
+                printf("Enter time taken by to complete it e.g 10:50 -> 1050");
+                printf("Solving Time Required : ");
+                scanf("%d", &e[fc].timeb); 
+                e[fc].ertb= e[fc].timeb; 
+                fc++;
+            } 
+            else
+            {
+                printf("PLEASE ENTER VALUE IN NUMBERS(e.g 1 to 9)");
+                printf("ENTER Query Id: "); 
+                scanf("%d", &po[sc].processid);
+                printf("ENTER Arrival Time: "); 
+                scanf("%d", &avg); 
+                if(avg<1000 || avg>1200)
+                {
+                    printf("\n!!! WRONG !!! Enter Correct TIME\n");
+                    takedata();
+                }
+                else 
+                {
+                    po[ace].timea= avg-1000; 
+                }
+                printf("Enter time taken by to complete it e.g 10:50 -> 1050");
+                printf("Solving Time Required: "); 
+                scanf("%d", &po[ace].timeb);  
+                po[ace].ertb= po[ace].timeb;
+                ace++;
             }
         }
-	}while(FG !=0);
-	
-    printf("\n|\tProcess Name\t  |\tArrival Time\t  |\tBurst Time\t |\tCompletion Time  \t|\n");
-
-    for(total = 0; total < av_process; total++)
-	{
-        WT = fac[total].com_y - fac[total].br_t - fac[total].av_t;
-
-        printf("\n|\t  %s\t    |\t  %d\t   |\t  %d\t   |\t  %d\t   |\n", fac[total].p_name, fac[total].av_t, fac[total].br_t, fac[total].com_y);
-    }	
-}
-
-void forstudent(int av_process)
-{
-	int total,av_t,br_t,qun;
-	struct avail_process fac[av_process];
-	
-	for(total = 0;total <av_process;total++){
-		printf("Process Detail for Process[%d]",total+1);
-		printf("\n");
-		printf("Enter name of Process : ");
-		scanf("%s",fac[total].pname);
-		printf("Enter ARRIVAL TIME : ");
-		scanf("%s",fac[total].av_t);
-		printf("Enter BRUST TIME : ");
-		scanf("%s",fac[total].br_t);	
-	}
-	printf("Enter QUANTUM TIME : ");
-	scanf("%d",&qun);
-	//
-	for(total = 0;total <av_process;total++)
-	{
-		for(int j= total +1 ; j<total;total++)
-		{
-			if(fac[total].av_t > fac[j].av_t)
-			{
-				obj = fac[count];
-				fac[count] = fac[j];
-				fac[j] = obj;
-			}
-		}
-	}
-	//
-	for(total = 0;total <av_process ; total++){
-		fac[total].rem = fac[total].br_t;
-		fac{total}.com_y = 0;
-	}
-	int TT,Q,RR[20];
-	TT = 0;
-	Q = 0;
-	RR[Q] = 0;
-	int FG,X,N,Z,WT=0;
-	do
-	{
-		for(total = 0;total<av_process;total++)
-		{
-			if(TT >= fac[av_pocess].av_t)
-			{
-				Z=0;
-				for(X=0;X<=Q;X++)
-				{
-					if(RR[X] == av_process)
-					{
-						Z++;
-					}
-				}
-				if(Z == 0)
-				{
-					Q++;
-					RR[Q] == av_process;
-				}	
-			}
-		}
-		if(Q == 0)
-		{
-			N = 0;
-		}
-		if(fac[av_process].rem == 0)
-		{
-			N++;
-		}
-		if(N>Q)
-		{
-			N = (N-1)%Q;
-		}
-		if(N<=Q)
-		{	if(fac[N].rem > 0)
-			{
-				if(fac[N].rem <qun)
-				{
-					TT += fac[N].rem;
-					fac[N].rem=0;
-				}
-				else
-				{
-					TT += qun;
-					fac[N].rem -= qun;
-				}
-				fac[N].com_y = TT;
-			}
-			N++;		
-		}
-		FG=0;
-		
-		for(count = 0; count < av_process; count++) 
-		{
-            if(fac[av_process].rem > 0) 
-			{
-                FG++;
-            }
-        }
-	}while(FG !=0);
-	
-    printf("\n|\tProcess Name\t  |\tArrival Time\t  |\tBurst Time\t |\tCompletion Time  \t|\n");
-
-    for(total = 0; total < av_process; total++)
-	{
-        WT = fac[total].com_y - fac[total].br_t - fac[total].av_t;
-
-        printf("\n|\t  %s\t    |\t  %d\t   |\t  %d\t   |\t  %d\t   |\n", fac[total].p_name, fac[total].av_t, fac[total].br_t, fac[total].com_y);
-    }	
+    }
 }
 
 
-int main()
-{
-	int type;
-	int available_process;
-	printf("     ENTER 1 OR 2      ");
-	printf("For STUDENT -> Enter 1");
-	printf("For FACULTY -> Enter 2");
-	scanf("%d",&type);
-	
-	switch(type)
-	{
-		case 1:
-			printf("   STUDENT TYPE IS SELECTED   ");
-			printf("\n\n");
-			printf("Enter No of Process for STUDENT : ");
-			scanf("%d",&available_process);
-			forfaculty(available_process);
-			break;
-		case 2:
-			printf("   FACULTY TYPE IS SELECTED   ");
-			printf("\n\n");
-			printf("Enter No of Process for FACULTY : ");
-			scanf("%d",&available_process);
-			forstudent(available_process);	
-			break;
-		default:
-			printf("Wrong option Selected Sorry");
-			printf("Re-Run AGAIN");			
-	}
-	return 0;	
+
+ int main()
+ {
+    takedata();
+    combine();
+    DoRR();
+    giveoutput();
 }
