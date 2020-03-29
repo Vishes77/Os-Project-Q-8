ebed366e133d4237e6391a85e9f9a7c827361202
Vishes77 <42653296+Vishes77@users.noreply.github.com>
2020-03-29 21:41:08 +0530

    error Free now

--- question8.c
+++ question8.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 struct Selection{
     int processid;
     int timea;
@@ -6,72 +7,33 @@ struct Selection{
     int completet;
     int ertb;
 }e[100], po[100], ko[100];
+// n is lest
+int n, fc=0, ace=0, nak=0;
+int quantamtime;
 
-void takedata(){
-    int val,  i, avg;
-    printf("Enter total Process (IN NUMBERS): "); 
-    scanf("%d", &n);
-    if(n==0) 
-    { 
-        printf("\n You have Enterd 0 \n"); 
-    }
-    else
-    {
-        printf("\n Now Enter The QUANTUM TIME for each Process : "); 
-        scanf("%d", &quantamtime);
-        printf("\nFor FACULTY( PRESS 1 ) \n");
-        printf("\nFor STUDENT( PRESS 2 ) \n");
-        for(i=0; i<n; i++)
-        {
-            printf("\n YOUR CHOICE ( 1 or 2)  : "); 
-            scanf("%d", &val);
-            if(val==1)
-            {
-                printf("PLEASE ENTER VALUE IN NUMBERS(e.g 1 to 9)");
-                printf("ENTER Query Id : "); 
-                scanf("%d", &e[fc].processid);
-                printf("ENTER Arrival Time: "); 
-                scanf("%d", &avg);
-                if(avg<1000 || avg>1200)
-                {
-                    printf("WRITE IN FORMAT LIKE e.g 10:30  -> 1030 \n");
-                    printf("\n!!! WRONG !!! Enter Correct TIME");
-                    takedata();
-                }
-                else
-                {
-                    e[fc].timea= avg-1000;
-                }
-                printf("Enter time taken by to complete it e.g 10:50 -> 1050");
-                printf("Solving Time Required : ");
-                scanf("%d", &e[fc].timeb); 
-                e[fc].ertb= e[fc].timeb; 
-                fc++;
-            } 
-            else
-            {
-                printf("PLEASE ENTER VALUE IN NUMBERS(e.g 1 to 9)");
-                printf("ENTER Query Id: "); 
-                scanf("%d", &po[sc].processid);
-                printf("ENTER Arrival Time: "); 
-                scanf("%d", &avg); 
-                if(avg<1000 || avg>1200)
-                {
-                    printf("\n!!! WRONG !!! Enter Correct TIME\n");
-                    takedata();
-                }
-                else 
-                {
-                    po[ace].timea= avg-1000; 
-                }
-                printf("Enter time taken by to complete it e.g 10:50 -> 1050");
-                printf("Solving Time Required: "); 
-                scanf("%d", &po[ace].timeb);  
-                po[ace].ertb= po[ace].timeb;
-                ace++;
+void DoRR(){
+    int tm= ko[0].timea, sym=0, ak=0, j, acr;
+    while(tm!=120 && ak!=nak){
+        for(j=0; j<=sym; j++){
+            if(ko[j].ertb > quantamtime){
+                tm += quantamtime;
+                ko[j].ertb -= quantamtime;
+            }
+            else if(ko[j].ertb <=quantamtime && ko[j].ertb !=0){
+                tm += ko[j].ertb;
+                ko[j].ertb =0;
+                ko[j].completet = tm;
+                ak++;
+            }
+            else;
+        }
+        int nry = sym+1;
+        for(acr= nry; acr<nak; acr++){
+            if(ko[acr].timea <= tm){
+                sym++;
             }
         }
-    }
+    }   
 }
 
 void combine()
@@ -135,30 +97,6 @@ void combine()
     }
 }
 
-void DoRR(){
-    int tm= ko[0].timea, sym=0, ak=0, j, acr;
-    while(tm!=120 && ak!=nak){
-        for(j=0; j<=sym; j++){
-            if(ko[j].ertb > quantamtime){
-                tm += quantamtime;
-                ko[j].ertb -= quantamtime;
-            }
-            else if(ko[j].ertb <=quantamtime && ko[j].ertb !=0){
-                tm += ko[j].ertb;
-                ko[j].ertb =0;
-                ko[j].completet = tm;
-                ak++;
-            }
-            else;
-        }
-        int nry = sym+1;
-        for(acr= nry; acr<nak; acr++){
-            if(ko[acr].timea <= tm){
-                sym++;
-            }
-        }
-    }   
-}
 void giveoutput(){
     int i=0, total=0, sum=0; 
     double avg;
@@ -175,9 +113,81 @@ void giveoutput(){
     printf("\nProcess Execution Complete");
 }
 
+void takedata(){
+    int val,  i, avg;
+    printf("Enter total Process (IN NUMBERS): "); 
+    scanf("%d", &n);
+    if(n==0) 
+    { 
+        printf("\n You have Enterd 0 \n"); 
+    }
+    else
+    {
+        printf("\n Now Enter The QUANTUM TIME for each Process : "); 
+        scanf("%d", &quantamtime);
+        printf("\nFor FACULTY( PRESS 1 ) \n");
+        printf("\nFor STUDENT( PRESS 2 ) \n");
+        for(i=0; i<n; i++)
+        {
+            printf("\n YOUR CHOICE ( 1 or 2)  : "); 
+            scanf("%d", &val);
+            if(val==1)
+            {
+                printf("PLEASE ENTER VALUE IN NUMBERS(e.g 1 to 9)");
+                printf("ENTER Query Id : "); 
+                scanf("%d", &e[fc].processid);
+                printf("ENTER Arrival Time: "); 
+                scanf("%d", &avg);
+                if(avg<1000 || avg>1200)
+                {
+                    printf("WRITE IN FORMAT LIKE e.g 10:30  -> 1030 \n");
+                    printf("\n!!! WRONG !!! Enter Correct TIME");
+                    takedata();
+                }
+                else
+                {
+                    e[fc].timea= avg-1000;
+                }
+                printf("Enter time taken by to complete it e.g 10:50 -> 1050");
+                printf("Solving Time Required : ");
+                scanf("%d", &e[fc].timeb); 
+                e[fc].ertb= e[fc].timeb; 
+                fc++;
+            } 
+            else
+            {
+                printf("PLEASE ENTER VALUE IN NUMBERS(e.g 1 to 9)");
+                printf("ENTER Query Id: "); 
+                scanf("%d", &po[ace].processid);
+                printf("ENTER Arrival Time: "); 
+                scanf("%d", &avg); 
+                if(avg<1000 || avg>1200)
+                {
+                    printf("\n!!! WRONG !!! Enter Correct TIME\n");
+                    takedata();
+                }
+                else 
+                {
+                    po[ace].timea= avg-1000; 
+                }
+                printf("Enter time taken by to complete it e.g 10:50 -> 1050");
+                printf("Solving Time Required: "); 
+                scanf("%d", &po[ace].timeb);  
+                po[ace].ertb= po[ace].timeb;
+                ace++;
+            }
+        }
+    }
+}
+
+
 
  int main()
  {
+    printf("\n INSTRUCTIONS \n"
+            "\n**>Enter tm in 2400 hours formtimea. example for 10:30 am enter 10030"
+            "\n**>Enter Query arrival tms in ascending order, i.e., in real tm arrival manner\n"
+            "\nAll tm units are in minutes. \n\n");
     takedata();
     combine();
     DoRR();
